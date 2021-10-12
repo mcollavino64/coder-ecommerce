8dd3344135fd93fa0cdbe46d75d69c3e5c944d99
Mariano Collavino <mcollavino64@gmail.com>
2021-10-11 21:28:49 -0300

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from './components/NavBar.js'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import {Nav, Navbar, NavDropdown} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NavBar/>
+      <div className="content">
+          Esto es el contenido.
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import logo from '../logo.svg';
+import 'bootstrap/dist/css/bootstrap.css'
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap'
+
+export const NavBar = () =>{
+    return  (<Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
+    <Navbar.Brand>
+      <img src={logo} width="40px" height="40px" />
+      Logo
+    </Navbar.Brand>
+
+    <Navbar.Toggle/>        
+    <Navbar.Collapse>
+      <Nav>
+        <NavDropdown title="Products"> 
+          <NavDropdown.Item href="#products/tea" >Tea</NavDropdown.Item>
+          <NavDropdown.Item href="#products/coffe" >Coffe</NavDropdown.Item>
+          <NavDropdown.Item href="#products/chocolate" >Chocolate</NavDropdown.Item>
+          <NavDropdown.Divider/>
+          <NavDropdown.Item href="#products/promo" >Promotion</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#blog"> Blog </Nav.Link>
+        <Nav.Link href="#about-us"> About Us </Nav.Link>
+        <Nav.Link href="#contact-us"> Contact Us </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>      
+  </Navbar>
+    )
+}
